e6154646fc4be773de84cba6f6185f4a3815248c
Vishal Mahajan <vishal423@gmail.com>
2018-12-07 12:54:39 +0530

    Templatized LICENSE

--- generators/app/index.js
+++ generators/app/index.js
@@ -5,14 +5,21 @@ const Generator = require('yeoman-generator');
 const packageJson = require('../../package.json');
 
 module.exports = class extends Generator {
+  constructor(args, opts) {
+    super(args, opts);
+
+    this.argument('name', { type: String, required: false });
+    this.argument('email', { type: String, required: false });
+  }
+
   initializing() {
     this.log(
       `${chalk.green(`
-     ████████      ████████    ████████     ██████████          ██████████    ██████████    ████      ██
-    ██      ██    ██      ██   ██     ██    ██                  ██            ██            ██  ██    ██
-    ██            ██      ██   ██      ██   ███████     █████   ██    ████    ███████       ██    ██  ██
-    ██      ██    ██      ██   ██     ██    ██                  ██      ██    ██            ██      ████
-     ████████      ████████    ████████     ██████████          ██████████    ██████████    ██        ██    
+     ████████     ████████    ███████      █████████           ██████████   █████████   ███       ██
+    ██      ██   ██      ██   ██     ██    ██                  ██           ██          ██ ██     ██
+    ██           ██      ██   ██      ██   ███████     █████   ██    ████   ███████     ██   ██   ██
+    ██      ██   ██      ██   ██     ██    ██                  ██      ██   ██          ██     ██ ██
+     ████████     ████████    ███████      █████████           ██████████   █████████   ██       ███    
 `)}`
     );
     this.log(
@@ -20,11 +27,38 @@ module.exports = class extends Generator {
         `v${packageJson.version}`
       )} generator!`
     );
+
+    this.contextOptions = {
+      name: this.options.name || this.config.get('name'),
+      email: this.options.email || this.config.get('email')
+    };
   }
 
-  prompting() {
+  async prompting() {
+    if (!this.contextOptions.name) {
+      const answer = await this.prompt({
+        type: 'input',
+        name: 'name',
+        message: 'Specify your full name > '
+      });
+      this.contextOptions.name = answer.name;
+    }
+
+    if (!this.contextOptions.email) {
+      const answer = await this.prompt({
+        type: 'input',
+        name: 'email',
+        message: 'Specify your email address > '
+      });
+      this.contextOptions.email = answer.email;
+    }
+  }
+
+  configuring() {
     this.config.set({
-      version: packageJson.version
+      version: packageJson.version,
+      name: this.contextOptions.name,
+      email: this.contextOptions.email
     });
   }
 
@@ -37,17 +71,15 @@ module.exports = class extends Generator {
     );
     this.fs.copy(this.templatePath('tests/.*'), this.destinationPath('./__tests__'));
 
-    this.fs.copyTpl(this.templatePath('dynamic/LICENSE'), this.destinationPath(`./LICENSE`), {});
+    this.fs.copyTpl(
+      this.templatePath('dynamic/LICENSE'),
+      this.destinationPath(`./LICENSE`),
+      this.contextOptions
+    );
 
     this.fs.copyTpl(
       this.templatePath('dynamic/package.json'),
       this.destinationPath(`./package.json`),
-      {}
-    );
-
-    this.fs.copyTpl(
-      this.templatePath('dynamic/README.md'),
-      this.destinationPath(`./README.md`),
       {}
     );
 
